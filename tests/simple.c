ba64db26aba3e813720e828338b31d10318a0315
Andrei Lascu <andrei.lascu@kcl.ac.uk>
2022-01-27 14:40:39 +0000

    Add missing content

---
+++ tests/simple.c
@@ -0,0 +1,28 @@
+#include "cheriintrin.h"
+
+/*******************************************************************************
+ * Globals and constants
+ ******************************************************************************/
+
+const size_t switcher_mem_max_size;
+
+/*******************************************************************************
+ * Extern functions
+ ******************************************************************************/
+
+extern int switch_compartment();
+extern void init_compartments(uint8_t*, uintptr_t);
+
+/*******************************************************************************
+ * Main
+ ******************************************************************************/
+
+int
+main()
+{
+    uint8_t* switcher_start = malloc(switcher_mem_max_size);
+    uintptr_t switch_comp_addr = &switch_compartment;
+
+    init_compartments(switcher_start, switch_comp_addr);
+    return 0;
+}
