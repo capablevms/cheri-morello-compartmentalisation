f67d3db978b21db11b0a51b3f493db4b30ab16d9
Andrei Lascu <andrei.lascu@kcl.ac.uk>
2022-01-28 16:16:35 +0000

    Various additions
    * add `clean` function to `switcher.S`
    * define global variables in `manager.S`
    * initialize addresses on heap for variables from `manager.S` in
      `simple.c`
    * simple static asserts for compartment value offsets in `simple.c`

--- tests/simple.c
+++ tests/simple.c
@@ -1,7 +1,14 @@
+#include "assert.h"
 #include "stdint.h"
 #include "stdlib.h"
 
 #include "cheriintrin.h"
+
+#include "comps_offsets.h"
+
+static_assert(COMP_SIZE == sizeof(void* __capability) * 3, "Invalid `COMP_SIZE` provided");
+static_assert(COMP_OFFSET_DDC == sizeof(void* __capability) * 1, "Invalid `COMP_OFFSET_DDC` provided.");
+static_assert(COMP_OFFSET_STK_ADDR == sizeof(void* __capability) * 2, "Invalid `COMP_OFFSET_STK_LEN` provided.");
 
 /*******************************************************************************
  * Globals and constants
@@ -16,6 +23,9 @@ const size_t switcher_mem_max_size;
 extern int switch_compartment();
 extern void init_compartments(uint8_t*, uintptr_t);
 
+extern void* comps_addr;
+extern void* switcher_caps;
+
 /*******************************************************************************
  * Main
  ******************************************************************************/
@@ -23,6 +33,9 @@ extern void init_compartments(uint8_t*, uintptr_t);
 int
 main()
 {
+    switcher_caps = malloc(sizeof(void* __capability) * 2);
+    comps_addr = malloc(COMP_SIZE * 10);
+
     uint8_t* switcher_start = malloc(switcher_mem_max_size);
     uintptr_t switch_comp_addr = (uintptr_t) switch_compartment;
 
