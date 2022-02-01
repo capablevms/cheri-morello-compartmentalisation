2d6913c7bcdc34165f3e9013ada02e7e038ea4d7
Andrei Lascu <andrei.lascu@kcl.ac.uk>
2022-02-01 15:13:39 +0000

    Updates
    * add wrapper to save `clr` when coming from C into `asm`
    * fix some constants in `simple.c` referring to memory region sizes
    * more work on `initialize_compartments()`

--- tests/simple.c
+++ tests/simple.c
@@ -14,14 +14,16 @@ static_assert(COMP_OFFSET_STK_ADDR == sizeof(void* __capability) * 2, "Invalid `
  * Globals and constants
  ******************************************************************************/
 
-const size_t switcher_mem_max_size;
+const size_t max_comp_cnt = 2;
+const size_t switcher_mem_max_size = max_comp_cnt * COMP_SIZE;
 
 /*******************************************************************************
  * Extern functions
  ******************************************************************************/
 
+extern void asm_call_wrapper(void*, ...);
+extern void init_compartments(uint8_t*, size_t, uintptr_t);
 extern int switch_compartment();
-extern void init_compartments(uint8_t*, uintptr_t);
 
 extern void* comps_addr;
 extern void* switcher_caps;
@@ -34,11 +36,12 @@ int
 main()
 {
     switcher_caps = malloc(sizeof(void* __capability) * 2);
-    comps_addr = malloc(COMP_SIZE * 10);
+    comps_addr = malloc(COMP_SIZE * max_comp_cnt);
 
     uint8_t* switcher_start = malloc(switcher_mem_max_size);
     uintptr_t switch_comp_addr = (uintptr_t) switch_compartment;
 
-    init_compartments(switcher_start, switch_comp_addr);
+    asm_call_wrapper(init_compartments,
+                     switcher_start, switcher_mem_max_size, switch_comp_addr);
     return 0;
 }
