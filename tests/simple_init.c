4c26fa27a5f3b4f0ef743a04294a5f53aede26c4
Andrei Lascu <andrei.lascu@kcl.ac.uk>
2022-02-08 14:43:32 +0000

    Update `init_compartments` and `simple_init`
    As discussed, we move towards a more abstract model, including memory
    management in the assembly layer. This updates the initialiation
    function to not require any user input, and setup memory appropriately,
    for future use.
    
    Add checks to `simple_init` to sanity check functionality.

--- tests/simple_init.c
+++ tests/simple_init.c
@@ -1,6 +1,7 @@
-#include "assert.h"
-#include "stdint.h"
-#include "stdlib.h"
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/mman.h>
 
 #include "cheriintrin.h"
 
@@ -11,22 +12,14 @@ static_assert(COMP_OFFSET_DDC == sizeof(void* __capability) * 1, "Invalid `COMP_
 static_assert(COMP_OFFSET_STK_ADDR == sizeof(void* __capability) * 2, "Invalid `COMP_OFFSET_STK_LEN` provided.");
 
 /*******************************************************************************
- * Globals and constants
- ******************************************************************************/
-
-const size_t max_comp_cnt = 2;
-const size_t switcher_mem_max_size = max_comp_cnt * COMP_SIZE;
-
-/*******************************************************************************
  * Extern functions
  ******************************************************************************/
 
-extern void asm_call_wrapper(void*, ...);
-extern void init_compartments(uint8_t*, size_t, uintptr_t);
-extern int switch_compartment();
+extern void* init_compartments();
+extern void* __capability * switcher_caps;
 
-extern void* comps_addr;
-extern void* switcher_caps;
+extern void switcher_entry();
+extern void switch_compartment_end();
 
 /*******************************************************************************
  * Main
@@ -35,15 +28,21 @@ extern void* switcher_caps;
 int
 main()
 {
-    switcher_caps = malloc(sizeof(void* __capability) * 2);
-    comps_addr = malloc(COMP_SIZE * max_comp_cnt);
+    void* inner_addr = init_compartments();
 
-    uint8_t* switcher_start = malloc(switcher_mem_max_size);
-    uintptr_t switch_comp_addr = (uintptr_t) switch_compartment;
+    assert(inner_addr != MAP_FAILED);
+    assert(switcher_caps == inner_addr);
 
-    // init_compartments();
+    void* __capability switcher_ddc = switcher_caps[0];
+    assert(cheri_is_valid(switcher_ddc));
+    assert(cheri_length_get(switcher_ddc) ==
+            COMP_SIZE * MAX_COMP_COUNT + 2 * sizeof(void* __capability));
 
-    asm_call_wrapper(init_compartments,
-                     switcher_start, switcher_mem_max_size, switch_comp_addr);
+    void* __capability switcher_pcc = switcher_caps[1];
+    assert(cheri_is_valid(switcher_pcc));
+    assert(cheri_address_get(switcher_pcc) == (unsigned long) switcher_entry);
+    assert(cheri_address_get(switcher_pcc) + cheri_length_get(switcher_pcc) ==
+            (unsigned long) switch_compartment_end);
+
     return 0;
 }
