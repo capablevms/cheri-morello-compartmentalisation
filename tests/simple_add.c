96a72ee6eec2df7bf431e48c6b3fec445960dfd8
Andrei Lascu <andrei.lascu@kcl.ac.uk>
2022-02-09 13:59:22 +0000

    Finalize `simple_add` test

--- tests/simple_add.c
+++ tests/simple_add.c
@@ -1,6 +1,7 @@
-#include "assert.h"
-#include "stdint.h"
-#include "stdlib.h"
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/mman.h>
 
 #include "cheriintrin.h"
 
@@ -11,23 +12,14 @@ static_assert(COMP_OFFSET_DDC == sizeof(void* __capability) * 1, "Invalid `COMP_
 static_assert(COMP_OFFSET_STK_ADDR == sizeof(void* __capability) * 2, "Invalid `COMP_OFFSET_STK_LEN` provided.");
 
 /*******************************************************************************
- * Globals and constants
- ******************************************************************************/
-
-const size_t max_comp_cnt = 2;
-const size_t switcher_mem_max_size = max_comp_cnt * COMP_SIZE;
-
-/*******************************************************************************
  * Extern functions
  ******************************************************************************/
 
-extern void asm_call_wrapper(void*, ...);
-extern void init_compartments(void*, size_t, void*);
-extern void add_compartment(void*, size_t, void*);
-extern int switch_compartment();
+extern void* __capability * comps_addr;
+extern size_t comps_cnt;
 
-extern void* comps_addr;
-extern void* switcher_caps;
+extern void* init_compartments();
+extern void* add_compartment(size_t, void*);
 
 /*******************************************************************************
  * Main
@@ -38,19 +30,20 @@ int comp_f_fn();
 int
 main()
 {
-    switcher_caps = malloc(sizeof(void* __capability) * 2);
-    comps_addr = malloc(COMP_SIZE * max_comp_cnt);
+    init_compartments();
 
-    void* switcher_start = malloc(switcher_mem_max_size);
-    void* switch_comp_addr = switch_compartment;
+    size_t comp_size = 2000;
+    assert(add_compartment(comp_size, comp_f_fn) != MAP_FAILED);
 
-    asm_call_wrapper(init_compartments,
-                     switcher_start, switcher_mem_max_size, switch_comp_addr);
+    assert(comps_cnt == 1);
 
-    const size_t comp_f_size = 1000;
-    uintptr_t comp_f_start = (uintptr_t) malloc(comp_f_size);
-    asm_call_wrapper(add_compartment,
-                     comp_f_start, comp_f_size, comp_f_fn);
+    void* __capability comp_ddc = comps_addr[0];
+    assert(cheri_is_valid(comp_ddc));
+    assert(cheri_length_get(comp_ddc) == comp_size);
+
+    void* __capability comp_pcc = comps_addr[1];
+    assert(cheri_is_valid(comp_pcc));
+    assert(cheri_address_get(comp_pcc) == (unsigned long) comp_f_fn);
 
     return 0;
 }
