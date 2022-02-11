c804e0963660c996f7d85a4c5cb2eb449196f585
Andrei Lascu <andrei.lascu@kcl.ac.uk>
2022-02-11 11:30:13 +0000

    Polish pass over `init` and `add` implementations
    * Add checks to ensure we don't add more compartments than we allocated
      space for
    * Small optimisations and unneeded code removal
    
    Remove comment
    
    Simplify
    
    Replace multiple instruction with a single one
    
    Remove unimplemented stub
    
    Optimization
    
    Better register usage
    
    Fix
    
    Fix
    
    Forgot to update checked register
    
    Removed unused function
    
    Remove an unneeded call
    
    Remove unneeded instruction

--- include/comps_offsets.h
+++ include/comps_offsets.h
@@ -1,6 +1,6 @@
 #define COMP_SIZE 48
-#define COMP_OFFSET_PCC 0
-#define COMP_OFFSET_DDC 16
+#define COMP_OFFSET_DDC 0
+#define COMP_OFFSET_PCC 16
 #define COMP_OFFSET_STK_ADDR 32
 
 #define MAX_COMP_COUNT 2
--- tests/simple_add.c
+++ tests/simple_add.c
@@ -8,7 +8,8 @@
 #include "comps_offsets.h"
 
 static_assert(COMP_SIZE == sizeof(void* __capability) * 3, "Invalid `COMP_SIZE` provided");
-static_assert(COMP_OFFSET_DDC == sizeof(void* __capability) * 1, "Invalid `COMP_OFFSET_DDC` provided.");
+static_assert(COMP_OFFSET_DDC == 0, "Invalid `COMP_OFFSET_DDC` provided.");
+static_assert(COMP_OFFSET_PCC == sizeof(void* __capability) * 1, "Invalid `COMP_OFFSET_PCC` provided.");
 static_assert(COMP_OFFSET_STK_ADDR == sizeof(void* __capability) * 2, "Invalid `COMP_OFFSET_STK_LEN` provided.");
 
 /*******************************************************************************
